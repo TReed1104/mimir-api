ea24a413c28e11e02c37f58d4f7ccac81eb41df5
Tom Reed <t.reed1104@gmail.com>
2020-11-27 10:46:01 +0000

    Amended the mysql init file

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,2 @@
-  
-CREATE DATABASE IF NOT EXISTS db_flask_api;
-CREATE DATABASE IF NOT EXISTS testing_db_flask_api;
+CREATE DATABASE IF NOT EXISTS db_mimir_api;
+CREATE DATABASE IF NOT EXISTS testing_db_mimir_api;
